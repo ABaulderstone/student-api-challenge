75f2055a5cd6c24ea7e5c95ac5d5036e9f82b9cd
Alex Baulderstone <alex.baulderstone@gmail.com>
2020-01-30 10:06:31 +1100

    created student model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,24 @@
 class StudentsController < ApplicationController
+    
+    def index 
+    
+    end 
+
+    def create 
+
+    end 
+    
+    def show 
+
+    end 
+
+    def update 
+
+    end 
+
+    def destroy 
+
+    end 
+
+
 end
