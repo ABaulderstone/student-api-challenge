3c9fe217e0acb95f80a936f2b8054e3e1dce26d4
Alex Baulderstone <alex.baulderstone@gmail.com>
2020-01-30 10:16:35 +1100

    added set_student and student_params as private methods in student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,14 @@
 class StudentsController < ApplicationController
+    before_action :set_student, except: [:index, :create]
     
     def index 
+        @students = Student.all
     
     end 
 
-    def create 
+    def create
+        @student = Student.new(student_params)
+        @student.save  
 
     end 
     
@@ -19,6 +23,16 @@ class StudentsController < ApplicationController
     def destroy 
 
     end 
+
+    private 
+    def set_student
+        id = params[:id]
+        @student = Student.find(id)
+    end  
+
+    def student_params
+        params.require(:student).permit(:name, :age, :gender, :bio)
+    end
 
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,3 @@
 class Student < ApplicationRecord
+    enum :gender {non_binary: 0, female: 1, male: 2, other: 3}
 end
